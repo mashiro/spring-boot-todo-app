2291319a4369a2ddc1671502e236fcc29f165fc8
mashiro <mail@mashiro.org>
2021-01-25 14:53:37 +0900

    Enable CORS on local env

--- src/main/kotlin/com/example/todoapp/presentation/rest/configurations/WebConfiguration.kt
+++ src/main/kotlin/com/example/todoapp/presentation/rest/configurations/WebConfiguration.kt
@@ -2,12 +2,22 @@ package com.example.todoapp.presentation.rest.configurations
 
 import com.example.todoapp.presentation.rest.converters.StringToULIDConverter
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.format.FormatterRegistry
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfiguration : WebMvcConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
         registry.addConverter(StringToULIDConverter())
+    }
+}
+
+@Configuration
+@Profile("local")
+class LocalWebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
     }
 }
