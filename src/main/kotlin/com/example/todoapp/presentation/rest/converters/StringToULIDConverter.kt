79db83e7275b9de89e43a576420ee6fded0a9421
mashiro <mail@mashiro.org>
2021-01-26 10:10:24 +0900

    Register with Component

--- src/main/kotlin/com/example/todoapp/presentation/rest/converters/StringToULIDConverter.kt
+++ src/main/kotlin/com/example/todoapp/presentation/rest/converters/StringToULIDConverter.kt
@@ -2,7 +2,9 @@ package com.example.todoapp.presentation.rest.converters
 
 import com.example.todoapp.domain.resources.base.ULID
 import org.springframework.core.convert.converter.Converter
+import org.springframework.stereotype.Component
 
+@Component
 class StringToULIDConverter : Converter<String, ULID> {
     override fun convert(source: String): ULID {
         // ULID is String typealias
